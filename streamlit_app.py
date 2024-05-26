8dee57aa480e26e1ad55a3f2b0adcb05c8d3e349
nodbew <165175929+nodbew@users.noreply.github.com>
2024-05-27 08:47:46 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,3 +6,7 @@ if 'settings' not in st.session_state:
     st.session_state.settings = init.settings()
 if 'OperandGenerator' not in st.session_state:
     st.session_state.OperandGenerator = init.operand_generator()
+if 'input' not in st.session_state:
+    st.session_state.input = []
+if 'input_for_format' not in st.session_state:
+    st.session_state.input_for_format = []
