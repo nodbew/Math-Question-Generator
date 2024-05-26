6b0867b71e326e8700ee135fea5b433b1d24e289
nodbew <165175929+nodbew@users.noreply.github.com>
2024-05-26 18:47:43 +0900

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+import core.initialize as init
+
+if 'settings' not in st.session_state:
+    st.session_state.settings = init.settings()
+if 'OperandGenerator' not in st.session_state:
+    st.session_state.OperandGenerator = init.operand_generator()
