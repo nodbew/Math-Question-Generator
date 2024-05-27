0dcc531c9fceab6a5b4a1c34817771f3850abde3
nodbew <165175929+nodbew@users.noreply.github.com>
2024-05-27 09:28:54 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 
-import core.initialize as init
+import core.core.initialize as init
+import core.components.initialize as comp_init
 
 if 'settings' not in st.session_state:
     st.session_state.settings = init.settings()
@@ -10,3 +11,7 @@ if 'input' not in st.session_state:
     st.session_state.input = []
 if 'input_for_format' not in st.session_state:
     st.session_state.input_for_format = []
+if 'input_keyboard' not in st.session_state:
+    st.session_state.input_keyboard = comp_init.default_keyboard()
+if 'format_input_keyboard' not in st.session_state:
+    st.session_state.format_input_keybaord = comp_init.default_format_keyboard()
