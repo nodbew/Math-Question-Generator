2fc35f69ede15e5680e4a77e2cdec610235ca0c3
nodbew <165175929+nodbew@users.noreply.github.com>
2024-05-27 09:14:23 +0900

    Update inputter.py

--- core/components/inputter.py
+++ core/components/inputter.py
@@ -1,8 +1,50 @@
-def _input(input:str) -> str|int:
+import numpy as np
+
+from ..data import signs
+
+def _input(input:str|int, target:str) -> None:
     '''
     Adds the input to st.session_state.*(Assigned in core.components.keyboard.Keyboard.__init__, when creating st.button)
+    Numbers will be automatically concatenated.
     '''
-    return input
+    if input in signs.BRACKETS:
+        st.session_state[target].append(input)
+        return
+    
+    if isinstance(input, int):
+        try:
+            if isinstance(st.session_state[target][-1], int):
+                st.session_state[target][-1] *= 10
+                st.session_state[target][-1] += input # 1,0 -> 10
+                
+            else:
+                # New number sequence
+                if st.session_state[target][-1] not in ('.', '{'):
+                    st.session_state[target].append('{')    
+                
+                if st.session_state[target][-1] not in signs.OPERANDS_AND_FUNCTIONS:
+                    st.session_state[target].append('*')    
+                    
+                st.session_state[target].append(input)
+                return
+                
+        except IndexError:
+            st.session_state[target].append("{")
+            st.session_state[target].append(input)
+            return
+    
+    else:
+        try:
+            if isinstance(st.session_state[target][-1], int):
+                st.session_state[target].append('}')
+                st.session_state[target].append('*')   
+                
+            st.session_state[target].append(input)
+            return
+            
+        except IndexError:
+            st.session_state[target].append(input)
+            return
 
 def callback(input:str, target:str):
     '''
@@ -23,5 +65,6 @@ def callback(input:str, target:str):
         return _callback
     else:
         def _callback():
-            return _input(input)
+            _input(input, target)
+            return
         return _callback
