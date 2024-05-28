d0e87be4867cd86f9cb92ec13751634c0509828d
nodbew <165175929+nodbew@users.noreply.github.com>
2024-05-28 09:42:19 +0900

    Create setting_tab.py

---
+++ core/components/setting_tab.py
@@ -0,0 +1,21 @@
+import streamlit as st
+
+'''
+Modify settings about problem generation and answer interpreting.
+'''
+
+def change_setting(name):
+    st.session_state.settings[name] = not st.session_state.settings[name]
+
+def setting():
+    st.write('問題生成に関する設定を変更できます')
+
+    # Create checkboxes 
+    for name, value in st.session_state.settings.items():
+        st.checkbox(name, 
+                    value = value, 
+                    key = 'setting_checkbox_' + name,
+                    on_change = change_setting,
+                    args = (name,)')
+                   )
+    return
