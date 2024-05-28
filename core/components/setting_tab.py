55005ad8b2fab4cbad75221cab58b12f756adabb
nodbew <165175929+nodbew@users.noreply.github.com>
2024-05-28 09:42:28 +0900

    Update setting_tab.py

--- core/components/setting_tab.py
+++ core/components/setting_tab.py
@@ -8,7 +8,7 @@ def change_setting(name):
     st.session_state.settings[name] = not st.session_state.settings[name]
 
 def setting():
-    st.write('問題生成に関する設定を変更できます')
+    st.header('問題生成に関する設定を変更できます')
 
     # Create checkboxes 
     for name, value in st.session_state.settings.items():
