0bb2eb6edca54248354cc7665617f7765ae940bf
nodbew <165175929+nodbew@users.noreply.github.com>
2024-05-28 11:12:01 +0900

    Update answer.py

--- core/core/answer.py
+++ core/core/answer.py
@@ -27,10 +27,80 @@ def calculate_answer(evaluated_question):
                 raise SettingViolation('設定違反：虚数解')
 
         return answers
+        
+def parse(input:str = None):
+    '''
+    Parse the recorded user input and returns a latex string.
+    return ''.join(st.session_state._input)
+    '''
+    
+    if input is None:
+        input = st.session_state.input
+    else:
+        input = st.session_state[input]
+    
+    result = " ".join(str(elem) for elem in input)
+    result = result.replace("**", "^").replace('*', '').replace('I', 'i')
 
-def parse(answer:str) -> sy.Expr:
+    return result
+
+def _evaluate(input:str):
+    """
+    Formats a string into evaluateable form.
+    """
+    # Default
+    if input is None:
+        input = st.session_state.input
+    else:
+        input = st.session_state[input]
+
+    # Check that there is at least one character to evaluate
+    if input == []:
+        return ""
+        
+    value_str = ''.join(str(elem) for elem in input).replace('{', '(').replace('}', ')')
+
+    # Check unterminated parenthesis
+    if type(input[-1]) == int:
+        value_str += ')'
+
+    # Replace the latex log function with a pythonic function styled str
+    value_str = re.sub(r'[\\]log_\((\d+?)\)*\((\d+?)\)', r'sy.log(\2, \1)', value_str)
+    
+    # Replace degree with radian
+    value_str = re.sub(r'(\d+)^{[\\]circ}', 'sy.rad(\1)', value_str)
+        
+    # Replace latex sqrt with math.sqrt function
+    value_str = re.sub(r'[\\]sqrt\[2\]\((\d+?)\)', r'sy.sqrt(\1)', value_str)
+
+    # Replace constants
+    value_str = value_str.replace(r'\pi', 'sy.pi').replace('I', 'sy.I')
+
+    # Replace calculation signs
+    value_str = value_str.replace(r'\times', '*').replace(r'\div', '/').replace('^', '**')
+
+    return value_str
+
+def evaluate(input = None):
     '''
-    Takes a string that represents the user's answer, and returns an evaluate-able string that represents the answer.
+    Raises an error with the streamlit style.
     '''
-    # Memo °はキーボードでつける
-    return answer
+    value_str = _evaluate(input)
+
+    if value_str == '':
+        return ''
+        
+    try:
+         return eval(value_str, 
+                    {
+                        '__builtins__' : None, 
+                        'sy' : sy,
+                    },                         
+                    {
+                        'I' : sy.I # Imaginary
+                    } | st.session_state.current_template._characters[0]
+                )
+        
+    except SyntaxError as e:
+        st.error(f'無効な式です  \nエラー：Errored string:{value_str}  \nActual error message:{e}')
+        return
