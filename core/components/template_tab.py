27d10f3b1b236c5e964673046229ec432ccc6f4f
nodbew <165175929+nodbew@users.noreply.github.com>
2024-06-02 15:29:50 +0900

    Update template_tab.py

--- core/components/template_tab.py
+++ core/components/template_tab.py
@@ -10,28 +10,39 @@ def template():
     # Select template
     st.header('問題形式の選択')
     options = list(st.session_state.templates.keys())
-    st.selectbox(
+    template = st.selectbox(
         label = '問題形式',
         key = 'template_selectbox',
         options = options,
         index = options.index('二次方程式の求解'),
-        on_change = lambda:exec('st.session_state.current_template = st.session_state.templates[st.session_state[template_selectbox]]')
+        on_change = lambda:exec(f'st.session_state.current_template = st.session_state.templates[{template}]')
     )
 
     # Add template
     st.header('問題形式の追加')
-    st.write('$' + 
-             answer.parse('format_input') + 
-             '$')
+    st.write('$' + answer.parse('format_input') + '$')
   
     with st.empty():
         st.session_state.format_keyboard.place()
-    type_selectbox = st.selectbox(label = '問題の種類', options = ['普通の問題', '展開問題', '因数分解問題')
+
     name_input = st.input('問題形式の名前')
-    s
+    type_selectbox = st.selectbox(label = '問題の種類', options = ['普通の問題', '展開問題', '因数分解問題')
+    
     if st.button('追加'):
         try:
-            st.session_state.templates[name] = (eval(type_selectbox)(answer.parse('format_input')))
+            match type_selectbox:
+                case '普通の問題':
+                    cls = question.QuestionTemplate
+                case '展開問題':
+                    cls = question.ExpansionQuestionTemplate
+                case '因数分解問題':
+                    cls = question.FactorizationQuestionTemplate
+                case capture:
+                    st.error('内部エラーが発生しました;error during matching question type:invalid question type passed')
+                    st.stop()
+                    
+            st.session_state.templates[name] = cls(answer.format_evaluate('format_input'))
+            
         except SyntaxError:
             st.error('無効な形式です  \nカッコを閉じ忘れたりしていませんか？')
 
