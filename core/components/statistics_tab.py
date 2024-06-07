aea4c5fa1a61da9089d56a7d8ad153ac1b092015
nodbew <165175929+nodbew@users.noreply.github.com>
2024-06-07 12:45:03 +0900

    Update statistics_tab.py

--- core/components/statistics_tab.py
+++ core/components/statistics_tab.py
@@ -8,6 +8,5 @@ Show statistics.
 def statistics():
     st.write('累計回答数：' + str(st.session_state.count))
     st.write('累計時間：' + str(datetime.datetime.now() - st.session_state.start_time))
-    if st.button("更新"):
-        st.rerun()
+    if st.button("更新"):pass # for rerunning 
     return 
