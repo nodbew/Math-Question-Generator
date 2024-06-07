c0e905f140c91ba32b85c26e284fe6726fe2326a
nodbew <165175929+nodbew@users.noreply.github.com>
2024-06-07 18:50:26 +0900

    Update answer.py

--- core/core/answer.py
+++ core/core/answer.py
@@ -87,7 +87,9 @@ def format_evaluate(input:str = None) -> str:
     i = 0
     char_gen = generators.CharacterGenerator()
     fmt = re.split('([-+*/])', value_str)
+    st.write('Reached while statement')
     while i < len(fmt):
+        print(i)
         if fmt[i].strip().startswith('generators.SympyFunction('):
             fmt[i] = fmt[i].strip()
             
