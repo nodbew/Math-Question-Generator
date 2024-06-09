3eec9f99ff43a6907469b720e34d918a4a12a291
nodbew <165175929+nodbew@users.noreply.github.com>
2024-06-09 13:05:09 +0900

    Update initialize.py

--- core/core/initialize.py
+++ core/core/initialize.py
@@ -31,7 +31,7 @@ def polynomial_problem(element : int = 1, dimension : int = 2, raw_format = Fals
 
     # Format
     dimension += 1 # dimension will be used in range(), therfore it has to be added by one
-    random_symbol = st.session_state.OperandGenerator
+    random_symbol = st.session_state.OperatorGenerator
     format = [[generators.NumberGenerator(), '*', character, '**', dim, random_symbol.__next__]
                 for dim in range(dimension)
                 for character in characters] 
