a43f74c69c4db5222cc75a3eae1205bf0b748016
nodbew <165175929+nodbew@users.noreply.github.com>
2024-06-09 18:13:32 +0900

    Update prototype_inputter.py

--- core/components/prototype_inputter.py
+++ core/components/prototype_inputter.py
@@ -3,6 +3,25 @@ def _input(input:str|int, target:str) -> None:
     Adds the input to st.session_state[target](Assigned in core.components.keyboard.Keyboard.__init__, when creating st.button)
     Numbers will be automatically concatenated.
     '''
+    # If this is the first input
+    if len(st.session_state[target]) == 0:
+        if isinstance(input, int):
+            st.session_state[target].extend(['{', input])
+            return
+            
+        elif input == r'\Character':
+            next = st.session_state.format_input_CharacterGenerator.__next__()
+            st.session_state.format_keyboard.add_keys({next:next}, target = 'format_input')
+            st.session_state[target].extend(['{', input, '}'])
+            pass # Input will be changed to an alphabet, but the function does not end its process instantly
+        
+        elif isinstance(input, generators.NumberGenerator):
+            st.session_state[target].extend(['{', input, '}'])
+            return
+        
+        elif isinstance(input, generators.OperatorGenerator):
+            st.session_state[target].append(input)
+            return
 
     # The input is a number
     if isinstance(input, int):
@@ -11,12 +30,13 @@ def _input(input:str|int, target:str) -> None:
             st.session_state[target][-1] *= 10
             st.session_state[target][-1] += input
 
+        elif st.session_state[target] in (signs.OPERATORS + ('{',)):
+            st.session_state[target].extend(['{', input])
+
         # sin, (, 2π, ), 20 -> sin(2π)*{20
         else:
-            st.session_state[target].append('*')
-            st.session_state[target].append('{')
-            st.session_state[target].append(input)
-
+            st.session_state[target].extend(['*', '{', input])
+            
         return
 
     # Special inputs for creating format
@@ -24,20 +44,19 @@ def _input(input:str|int, target:str) -> None:
         next = st.session_state.format_input_CharacterGenerator.__next__()
         st.session_state.format_keyboard.add_keys({next:next}, target = 'format_input')
         input = next
+        pass # Input will be changed to an alphabet, but the function does not end its process instantly
         
     elif isinstance(input, generators.NumberGenerator):
-        st.session_state[target].append('{')
-        st.session_state[target].append(input)
-        st.session_state[target].append('}')
+        if st.session_state[target] in (signs.OPERATORS + ('{',)):
+            st.session_state[target].extend(['{', input, '}'])
+
+        # sin, (, 2π, ), 20 -> sin(2π)*{20
+        else:
+            st.session_state[target].extend(['*', '{', input, '}'])
         return
         
     elif isinstance(input, generators.OperatorGenerator):
         st.session_state[target].append(input)
-        return
-
-    elif isinstance(input, generators.SympyFunction):
-        st.session_state[target].append(input)
-        st.session_state[target].append('(')
         return
 
     # Special inputs for both format and normal input
@@ -45,5 +64,33 @@ def _input(input:str|int, target:str) -> None:
         st.session_state[target].append(input)
         return
 
-    elif input in signs.FUNCTIONS:
+    # Other cases
+    if input in signs.CALCULATION_SYMBOLS:
+        if isinstance(st.session_state[target][-1], int):
+            st.session_state[target].append('}')
+
         st.session_state[target].append(input)
+        return
+
+    elif input in signs.FUNCTIONS:
+        st.session_state[target].extend([input, '{', 0]) # There will be a number or a constant 
+        return
+
+    else:
+        if st.session_state[target][-1] == 0:
+            # See 'elif input in signs.FUNCTIONS' clause
+            # FUNCTION{, a should not be converted into FUNCTION{ (0 * a)
+            st.session_state[target].pop(-1)
+            st.session_state[target].extend(['{', input, '}'])
+            return
+            
+        elif isinstance(st.session_state[target][-1], int) or st.session_state[target][-1] in signs.CONSTANTS:
+            st.session_state[target].extend(['}', '*', '{', input])
+            return
+
+        elif st.session_state[target][-1] == '}':
+            st.session_state[target].extend(['*', '{', input])
+
+        else:
+            st.session_state[target].extend(['{', input])
+            return
