01d8553f0469d9958e3c1c3e52e2606b39f8c6d4
nodbew <165175929+nodbew@users.noreply.github.com>
2024-06-09 14:51:40 +0900

    Update inputter.py

--- core/components/inputter.py
+++ core/components/inputter.py
@@ -11,7 +11,7 @@ def _input(input:str|int, target:str) -> None:
     '''
     if input == r'\Character':
         next = st.session_state.format_input_CharacterGenerator.__next__()
-        st.session_state.format_keyboard.add_key({next:next})
+        st.session_state.format_keyboard.add_keys({next:next})
         input = next
         
     elif isinstance(input, generators.NumberGenerator):
