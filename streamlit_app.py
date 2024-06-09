17d123427365b51c63e5b92c1010d2a83261e0fe
nodbew <165175929+nodbew@users.noreply.github.com>
2024-06-09 14:46:48 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,7 +25,7 @@ if 'templates' not in st.session_state:
         '二次方程式の求解' : init.polynomial_problem(),
         '二次方程式の展開' : question.ExpansionQuestionFormat(init.polynomial_problem(raw_format = True)),
         '二次方程式の因数分解' : question.FactorizationQuestionFormat(init.polynomial_problem(raw_format = True)),
-        '整数問題' : question.NumberQuestionFormat([generators.NumberGenerator(), st.session_state.OperandGenerator, generators.NumberGenerator()])
+        '整数問題' : question.NumberQuestionFormat([generators.NumberGenerator(), st.session_state.OperatorGenerator, generators.NumberGenerator()])
     }
     
 if 'current_template' not in st.session_state:
