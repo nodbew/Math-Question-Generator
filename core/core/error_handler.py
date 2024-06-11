0c4b9b5e63092e55427ff9f471af9d64ac23d6f1
nodbew <165175929+nodbew@users.noreply.github.com>
2024-06-11 20:45:33 +0900

    Update error_handler.py

--- core/core/error_handler.py
+++ core/core/error_handler.py
@@ -1,6 +1,7 @@
 from functools import wraps
 
 import streamlit as st
+import sympy as sy
 '''
 def error_handler(func):
     @wraps(func)
@@ -11,6 +12,7 @@ def error_handler(func):
         except Exception as e:
             err_msg = f'エラーが起きました：{str(e)}  \n種別：{e.__class__}'
             st.error(err_msg)
+            return sy.nan
         return
 
     return _wrapper
