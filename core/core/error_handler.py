6a074abe0e9828e7da37348ea86fda98417c7ea4
nodbew <165175929+nodbew@users.noreply.github.com>
2024-09-16 16:59:07 +0900

    Added doc and removed dead codes

--- core/core/error_handler.py
+++ core/core/error_handler.py
@@ -4,15 +4,16 @@ import streamlit as st
 import sympy as sy
 
 def error_handler(func):
+    """
+    Captures any error and prints the error's type to the console
+    """
     @wraps(func)
     def _wrapper(*args, **kwargs):
         try:
             return func(*args, **kwargs)
             
         except Exception as e:
-            err_msg = f'エラーが起きました：{str(e)}  \n種別：{e.__class__}'
-            st.error(err_msg)
+            st.error(f'エラーが起きました：{e}  \n種別：{e.__class__}')
             return None
-        return
 
     return _wrapper
