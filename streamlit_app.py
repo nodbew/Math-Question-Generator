28724d3d6041723267b0986e772bb9dc6aede580
nodbew <165175929+nodbew@users.noreply.github.com>
2024-09-16 11:13:56 +0900

    Added doc for streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,22 +10,28 @@ from core.components.keyboard import Keyboard
 
 # Initialization of st.session_state
 if 'settings' not in st.session_state:
+    # user settings
     st.session_state.settings = init.settings()
 if 'OperandGenerator' not in st.session_state:
+    # rng-like commonized random operator gen
     st.session_state.OperatorGenerator = init.operator_generator()
 if 'input' not in st.session_state:
+    # inputs from keyboard
     st.session_state.input = []
 if 'input_keyboard' not in st.session_state:
+    # commonized keyboard object
     st.session_state.input_keyboard = Keyboard(comp_init.default_keyboard(), target = 'input')
 if 'format_input' not in st.session_state:
+    # inputs for format, from another keyboard
     st.session_state.format_input = []
 if 'format_keyboard' not in st.session_state:
+    # a keyboard for inputting question format
     st.session_state.format_keyboard = Keyboard(comp_init.default_format_keyboard(), target = 'format_input')
 if 'format_input_CharacterGenerator' not in st.session_state:
+    # character gen for format input
     st.session_state.format_input_CharacterGenerator = init.character_generator()
-if 'settings' not in st.session_state:
-    st.session_state.settings = init.settings()
 
+# templates in a dict
 if 'templates' not in st.session_state:
     st.session_state.templates = {
         '二次方程式の求解' : init.polynomial_problem(),
@@ -35,19 +41,25 @@ if 'templates' not in st.session_state:
     }
     
 if 'current_template' not in st.session_state:
+    # currently used templates
     st.session_state.current_template = st.session_state.templates['二次方程式の求解']
 if 'current_question' not in st.session_state:
+    # generate the first question
     st.session_state.current_question = st.session_state.current_template.generate()
 if 'checked' not in st.session_state:
+    # to respond to the "answer" button
     st.session_state.checked = False
 if 'count' not in st.session_state:
+    # global count of questions answered
     st.session_state.count = 0
 if 'correct_count' not in st.session_state:
+    # global count of questions correctly answered
     st.session_state.correct_count = 0
 if 'start_time' not in st.session_state:
+    # record of the init time
     st.session_state.start_time = datetime.datetime.now()
 
-# Tabs
+# Layouts of each tab are written in each file
 import core.components.main_tab as main
 import core.components.setting_tab as setting
 import core.components.template_tab as template
@@ -56,6 +68,7 @@ import core.components.statistics_tab as statistics
 # Create tab objects with names
 main_tab, template_tab, setting_tab, statistics_tab = st.tabs(['出題', '問題形式', '設定', '統計'])
 
+# See each file for detail
 with main_tab:
     main.main()
 with template_tab:
@@ -64,4 +77,3 @@ with setting_tab:
     setting.setting()
 with statistics_tab:
     statistics.statistics()
-
